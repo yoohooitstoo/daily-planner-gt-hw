ecf298ac970c785e9ea21f617ef4011b0ba5c017
yoohooitstoo <stumanscott@gmail.com>
2020-06-14 10:45:44 -0400

    added img to schedule and updated saveCol to a button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     //the first row of the time scheduler
     var timeRow = $("<div>").addClass("row");
     var timeCol = $("<div>").addClass("hour col-sm-2");
-    var saveCol = $("<div>").addClass("col-sm-2 saveBtn");
+    var saveCol = $("<button>").addClass("col-sm-2 saveBtn");
     var scheduleCol = $("<div>").addClass("col-sm-8 past present future");
 
 
@@ -27,8 +27,8 @@ $(document).ready(function () {
     $(".past").text("this is where the main text appears");
     //the save button
     scheduleCol.after(saveCol);
-    $(".saveBtn").text("this is where the save button goes");
-
+    $(".saveBtn").html('<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z"/></svg>');
+    saveCol.addClass(".saveBtn i:hover");
     // FOR LOOP TO CREATE NEW ROW
     var timeList = [
         "9 AM",
@@ -46,10 +46,10 @@ $(document).ready(function () {
     //        timeCol.append(timeList);
     //        }
     //var timeDisplay= timeList.length;
-    // var newRow = $('.row');
-    // for (var i = 0; i < 8; i++) {
-    //     newRow.clone().insertAfter(newRow);
-    // };
+    var newRow = $('.row');
+    for (var i = 0; i < 8; i++) {
+        newRow.clone().insertAfter(newRow);
+    };
 
 
 
