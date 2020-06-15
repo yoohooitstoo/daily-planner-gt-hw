50de5db684c425c70b93fd8c87d726ed3d2bba0f
yoohooitstoo <stumanscott@gmail.com>
2020-06-15 06:52:33 -0400

    Added in pseudocode, possible solution for the localStorage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//This allows tha html page to load before becoming dynamic thru js
 $(document).ready(function () {
     console.log("you did it");
 
@@ -5,13 +6,7 @@ $(document).ready(function () {
     // Display Date in the Jumbotron
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-
-    //Create Variables
-    //The container of the scheduler
-
-    // FOR LOOP TO CREATE NEW ROW
-    //var timeListindex = 0;
-
+    // Time list array
     var timeList = [
         "9 AM ",
         "10 AM",
@@ -35,35 +30,57 @@ $(document).ready(function () {
         //the first row of the time scheduler
         var hourList = timeList[i];
         //Row that stores all the material
-        var timeRow = $("<div class='row' >");
-        var timeCol = $("<div class= 'hour col-sm-1'>");
-        var saveCol = $("<button type='button' class= 'col-sm-1 saveBtn'>");
-        var scheduleCol = $("<textarea class= 'col-sm-10 past'>");
+        var timeRow = $("<div class='row'>");
+        //Column that displays the time
+        var timeCol = $("<div class= 'hour col-sm-2'>");
+        //Column that has the save icon
+        var saveCol = $("<button type='button' class= 'col-sm-2 saveBtn'>");
+        //Column for text area and for the color scheme
+        var scheduleCol = $("<textarea class= 'col-sm-8'>");
+        //Allows the timeCol to house the timeList
         timeCol.text(hourList);
+        //Has each column one after the other
         timeRow.append(timeCol).append(scheduleCol).append(saveCol);
+        //keeps all rows inside the container
         container.append(timeRow);
-        scheduleCol.after(saveCol);
+        //Inserts the floppy disc icon into the saveCol
         $(".saveBtn").html('<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z"/></svg>');
+        // Hopefully giving the saveCole the added class
         saveCol.addClass(".saveBtn i:hover");
-        
+
+
+        //Conditional statement to have timeCol display color
         if (hourList === currentTime) {
             scheduleCol.addClass("present");
-        } else if  (hourList < currentTime) {
+        }
+        if (hourList < currentTime) {
             scheduleCol.addClass("past");
-        } else{
+        } else {
             scheduleCol.addClass("future");
         };
-        //Condtiontional statement to have timeCol display color
+
+
+        //Local Storage
+        //Check Browser Support
+        // if (typeof(Storage) !== "undefined") {
+        //     // Store
+        //     localStorage.setItem("lastname", "");
+        //     // Retrieve
+        //     document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+        //   } else {
+        //     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+        //   }
+
         // var timeFrame = 
         //Create div 
         //Needs row with column
         // modify div / Append a div to the class="time-block"
-        saveCol.on("click", function (e) {
-            var scheduleEntry = scheduleCol.html();
-            console.log(scheduleCol);
-            event.preventDefault();
-            if (scheduleEntry.length > 0); {};
-                
+        // saveCol.on("click", function (e) {
+        //     var scheduleEntry = scheduleCol.html();
+        //     console.log(scheduleCol);
+        //     event.preventDefault();
+        //     if (scheduleEntry.length > 0); {};
+
 
         //$(".hour").text("");
         //the text area
@@ -82,15 +99,7 @@ $(document).ready(function () {
 
 
 
-    // Create Functions
-
-    //  We need to build
-
-
-    // Call the Functions
-
-
-    // Add EventListeners
+   
 
     // Use this function as Event listener
     // $("#click-me").on("click", function() {
